5d9f5f14759196c7c9627bfc25db9c593dd2216c
yusrilikhsan <80373173+yusrilikhsan@users.noreply.github.com>
2021-03-23 16:43:48 +0700

    Add files via upload

---
+++ haloindonesia.java
@@ -0,0 +1,14 @@
+/*
+ * nama; mochamad yusril ikhsan
+ * nim; 11200930000098
+ * kelas; 2D
+ */
+package indonesia;
+
+public class haloindonesia {
+    public static void main (String [] args) {
+        System.out.println ("Halo Jakarta");
+        System.out.println ("Selamat Pagi Indonesia");
+        System.out.println ("Jaya Indonesia");
+}
+}
